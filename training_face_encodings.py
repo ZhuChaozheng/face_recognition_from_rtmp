bf0059374b61dd7bfbb91a9f312ec99b4008c43c
ZhuChaozheng <370045744@qq.com>
2019-06-02 12:41:37 +0800

    increase command line operation

--- training_face_encodings.py
+++ training_face_encodings.py
@@ -27,11 +27,11 @@ print(known_model)
 #
 # print(rfc)
 #save model
-f = open('saved_model/rfc.pickle','wb')
+f = open('saved_model/rfc.yaml','wb')
 pickle.dump(known_model,f)
 f.close()
 #load model
-f = open('saved_model/rfc.pickle','rb')
+f = open('saved_model/rfc.yaml','rb')
 known_model_read = pickle.load(f)
 f.close()
 known_face_encodings, known_face_names = zip(*known_model_read)
@@ -41,22 +41,32 @@ print(known_face_encodings, known_face_names)
 #
 # # automatic scan specific folder, then save known_names, known_face_encodings
 # # to same folder as a yaml format file.
-# def scan_known_people(known_people_folder):
-#     known_names = []
-#     known_face_encodings = []
-#
-#     for file in image_files_in_folder(known_people_folder):
-#         basename = os.path.splitext(os.path.basename(file))[0]
-#         img = face_recognition.load_image_file(file)
-#         encodings = face_recognition.face_encodings(img)
-#
-#         if len(encodings) > 1:
-#             click.echo("WARNING: More than one face found in {}. Only considering the first face.".format(file))
-#
-#         if len(encodings) == 0:
-#             click.echo("WARNING: No faces found in {}. Ignoring file.".format(file))
-#         else:
-#             known_names.append(basename)
-#             known_face_encodings.append(encodings[0])
-#
-#     return known_names, known_face_encodings
+def scan_known_people(known_people_folder):
+    known_names = []
+    known_face_encodings = []
+
+    for file in image_files_in_folder(known_people_folder):
+        basename = os.path.splitext(os.path.basename(file))[0]
+        img = face_recognition.load_image_file(file)
+        encodings = face_recognition.face_encodings(img)
+
+        if len(encodings) > 1:
+            click.echo("WARNING: More than one face found in {}. Only considering the first face.".format(file))
+
+        if len(encodings) == 0:
+            click.echo("WARNING: No faces found in {}. Ignoring file.".format(file))
+        else:
+            known_names.append(basename)
+            known_face_encodings.append(encodings[0])
+
+    return known_names, known_face_encodings
+
+@click.command()
+@click.argument('known_people_folder')
+def main(known_people_folder):
+    known_names, known_encodings = scan_known_people(known_people_folder)
+    print(known_names, known_encodings)
+
+
+if __name__ == "__main__":
+    main()
